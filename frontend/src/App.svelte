fb4c322f63f76ca29e4b01c691a755c6e075993e
Thun (MacBook Pro) <bsthun@gmail.com>
2024-12-23 09:18:22 +0700

    update splash page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
+  import { Route, Router } from 'svelte-navigator'
   import './style/style.scss'
+  import Splash from "$/page/Splash.svelte";
+  import Status from "$/page/status/Status.svelte";
 </script>
 
 <main>
-  <h1>Mockswitch</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Router>
+    <Route path="/" component={Splash} />
+    <Route path="/status" component={Status} />
+  </Router>
 </main>
-
-<style>
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ frontend/src/page/Splash.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { Events } from '@wailsio/runtime'
+    import { navigate } from 'svelte-navigator'
+
+    Events.On('app', () => {
+        navigate('/detail', { replace: true })
+    })
+</script>
+
+<main class="h-full flex flex-col justify-between items-center">
+    <div></div>
+    <div class="flex flex-col items-center">
+        <img src="/appicon.png" alt="Runnr" width="96px" height="96px">
+        <h1 class="mt-2 text-2xl focus-visible:outline-none">
+            Mockswitch
+        </h1>
+    </div>
+    <div class="flex items-center mb-8 gap-2">
+        <img src="/connectedtech.png" alt="Connected Tech" width="32px">
+        <div class="border-r border-gray-300 h-8"></div>
+        <img src="/pixcee.png" alt="Connected Tech" width="32px">
+    </div>
+</main>
